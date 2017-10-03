64940b9de1f3cb5f259c2c2ec9d985fec47f73f7
ninap41 <ninapalumbo@gmail.com>
2017-10-03 08:53:06 -0500

    Fun With Functions

---
+++ FunWithFunctions/python.py
@@ -0,0 +1,40 @@
+# # num = 2000
+# for x in range(1, 2000):
+#     if x % 2 == 0:
+#         print "This is an even number"
+#     if x % 2 !=0:
+#         print "This is an odd number"    
+
+# # # # As Function
+# def oddEven(num):
+#     for x in range(1, 2000):
+#         if x % 2 == 0:
+#             print "This is an even number"
+#         if x % 2 !=0:
+#             print "This is an odd number"  
+# # # oddEven(num)  
+# a = [2,4,10,16]
+# for value in a:
+#     print value * 5
+# # # other way
+# a = [2,4,10,16]
+# b = multiply(a, 5)
+# def mutliply(a,b):
+#     for value in a:
+#             return b
+# hacker
+arr=[2,3,4,5]
+new_array = []
+for x in arr:
+    val_arr = []
+    for i in range(0,x):
+        val_arr.append(1)
+    new_array.append(val_arr)
+    print new_array
+
+
+
+   
+
+
+    
