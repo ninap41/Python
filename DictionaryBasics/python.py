8bbb75789083de0c65e178f799134c967abe7bb5
ninap41 <ninapalumbo@gmail.com>
2017-10-03 14:27:46 -0500

    Added Dictionary Basics

---
+++ DictionaryBasics/python.py
@@ -0,0 +1,12 @@
+Nina = {} #create an empty dictionary then add values
+Nina["full_name"] = "My name is Nina Marie Palumbo"
+Nina["age"] = "My age 25"
+Nina["country"] = "My Country is United States"
+Nina["favorite_language"] = "My favorite language is Javascript&JQuery"
+
+def dictionary_basics():
+    for data in Nina:
+        print  Nina[data]
+dictionary_basics()
+
+    
