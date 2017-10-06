66f1ceb38c0b0f5160596ec28987078e11ac1518
ninap41 <ninapalumbo@gmail.com>
2017-10-06 07:38:43 -0500

    added dojosurvey

---
+++ FlaskFundamentals/DojoSurvey/DojoSurvey.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/results', methods=['POST']) 
+def create_process():
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template("result.html", name = name, location = location, language = language, comment = comment)
+
+app.run(debug=True) 
