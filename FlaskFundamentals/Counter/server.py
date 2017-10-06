8c20df262bc78e455cabf3abb98b41d14fb2ef16
ninap41 <ninapalumbo@gmail.com>
2017-10-06 13:35:59 -0500

    added counter

--- FlaskFundamentals/Counter/server.py
+++ FlaskFundamentals/Counter/server.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
 app = Flask(__name__)
+app.secret_key = 'secretkey'
 
 
-@app.route('/', counter)
+@app.route('/')
 def index():
-    return ("counter.html")
+    if session.has_key('count') == False:
+        session['count'] = 0
+    return render_template('index.html', count= session['count'])
 
-@app.route('/', methods=['POST'])
-def index():
-    if request.form['action'] == 'visited':
-        visits+=
-    return render_template("counter.html", visits=visits)
+@app.route('/increment', methods=['POST'])
+def increment():
+    session['count'] += 2
+    return redirect('/')
 
-app.run(debug=True) 
+@app.route('/clear', methods=['POST'])
+def clear():
+    session['count'] = 0
+    return redirect('/')
+
+app.run(debug=True)
