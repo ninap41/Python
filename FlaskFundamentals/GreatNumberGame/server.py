001ecbca5dc6e4b3a213363776d9d26263a7749e
ninap41 <ninapalumbo@gmail.com>
2017-10-07 14:45:51 -0500

    added Great Numbers Game

---
+++ FlaskFundamentals/GreatNumberGame/server.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+@app.route
+
+@app.route('/')
+def index():
+    if session.has_key('correct_number') == False:
+        session['correct_number'] = random.randrange(1, 100) 
+        print session['correct_number']
+    else:
+        pass
+    print session['correct_number']
+    return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guessing():
+    user_guess = request.form['guess']
+    if int(user_guess) == session['correct_number']:
+        session['answer_state'] = 'Right'
+        session['user_guess'] = True
+
+        print 'you guessed it!'
+    elif int(user_guess) > session['correct_number']:
+        session['answer_state'] = 'High'
+        session['user_guess'] = int(user_guess)
+       
+    elif int(user_guess) < session['correct_number']:
+        session['answer_state'] = 'Low'
+        session['user_guess'] = int(user_guess)
+       
+        session['display_tryagain'] = str('You didnt do it')
+    else:
+        pass
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session['correct_number'] = 0
+    session['hide'] == True
+    return redirect('/')
+
+
+app.run(debug=True)
+
+
+
+
+#incomplete
+
